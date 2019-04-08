ee08134da584543cfcb501d8ec001952157dcdf6
Dmitry Davydov1 <dmitry_davydov1@epam.com>
2019-04-08 20:54:36 +0300

    HW08 base

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -1,42 +1,55 @@
-// $(document).ready(function () {
+const ajaxUrl = "ajax/admin/users/";
+let datatableApi;
+
+function enable(chkbox, id) {
+    const enabled = chkbox.is(":checked");
+    $.ajax({
+        url: ajaxUrl + id,
+        type: "POST",
+        data: "enabled=" + enabled
+    }).done(function () {
+        chkbox.closest("tr").attr("data-userEnabled", enabled);
+        successNoty(enabled ? "Запись активирована" : "Запись деактивирована");
+    }).fail(function () {
+        $(chkbox).prop("checked", !enabled);
+    });
+}
+
 $(function () {
-    makeEditable({
-            ajaxUrl: "ajax/admin/users/",
-            datatableApi: $("#datatable").DataTable({
-                "paging": false,
-                "info": true,
-                "columns": [
-                    {
-                        "data": "name"
-                    },
-                    {
-                        "data": "email"
-                    },
-                    {
-                        "data": "roles"
-                    },
-                    {
-                        "data": "enabled"
-                    },
-                    {
-                        "data": "registered"
-                    },
-                    {
-                        "defaultContent": "Edit",
-                        "orderable": false
-                    },
-                    {
-                        "defaultContent": "Delete",
-                        "orderable": false
-                    }
-                ],
-                "order": [
-                    [
-                        0,
-                        "asc"
-                    ]
-                ]
-            })
-        }
-    );
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "name"
+            },
+            {
+                "data": "email"
+            },
+            {
+                "data": "roles"
+            },
+            {
+                "data": "enabled"
+            },
+            {
+                "data": "registered"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "asc"
+            ]
+        ]
+    });
+    makeEditable();
 });
